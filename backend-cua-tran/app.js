793e0811724f1655d206303e52ee1e2e22567658
Hồ Đăng Dũng <65653599+HoDangDung@users.noreply.github.com>
2023-04-05 12:53:24 +0700

    delete admin user folder

--- backend-cua-tran/app.js
+++ backend-cua-tran/app.js
@@ -4,12 +4,10 @@ app.use(express.json());
 var controller = require(__dirname + "/apps/controllers");
 app.use(controller);
 
-app.set("views",__dirname + "/apps/views");
+app.set("views", __dirname + "/apps/views");
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/partical", express.static(__dirname + "/views/partical"));
-
-
 
 var server = app.listen(3000, function() {
     console.log("server is running");
